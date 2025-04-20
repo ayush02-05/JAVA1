d91710326c680b8c8fd4baf116397de52d96178f
ayush02-05 <lakheraayush171@gmail.com>
2025-04-20 23:14:01 +0530

    initial commit

---
+++ training/src/problem_1.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class problem_1 {
+    public static void main(String[] args) {
+        
+        // // Q1. Sum of All Elements ->  Ek array lo user se input me, aur uske sare elements ka sum nikaalo.
+        // int[] arr = {1,2,3,4,5};
+        // int sum = 0 ;
+        // for(int i=0 ;i<arr.length ; i++)
+        // {
+        //     sum += arr[i];
+        // }
+        // System.out.println(sum);
+
+        // // Q2. Find Maximum Element -> Array me sabse bada element kaunsa hai, wo print karo.
+        // int[] arr = {2,3,5,32,7,21};
+        // int large = arr[0] ,index=0;
+        // for(int i=1;i<arr.length ;i++)
+        // {
+        //     if(arr[i] > large)
+        //     {
+        //         large = arr[i];
+        //         index = i;
+        //     }
+        // }
+        // System.out.println("the largest element "+ large +" is at  the index of "+ index);
+
+        // // Q3. Count Even and Odd Numbers -> Array ke andar kitne even aur odd numbers hain, uska count nikaalo.
+        // int[] arr = {1,2,3,4,5};
+        // int even=0,odd=0;
+        // for(int i=0 ;i<arr.length ;i++)
+        // {
+        //     if(arr[i] % 2 == 0)
+        //     {
+        //         even++;
+        //     }
+        //     else{
+        //         odd++;
+        //     }
+        // }
+        // System.out.println("number of even: " + even + " number of odd: " + odd );
+
+        // // Q4. Linear Search-> User se ek number lo, aur check karo wo number array me hai ya nahi.
+        // int[] arr = {2,3,5,32,7,21};
+        // Scanner in = new Scanner(System.in);
+        // System.out.print("enter the key you want to search linearly : ");
+        // int index = -1 ;
+        // int n = in.nextInt();
+        // for(int i=0 ;i<arr.length ;i++)
+        // {
+        //     if(arr[i] == n)
+        //     {
+        //         index = i;
+        //         System.out.println("the key "+ n + " is found at index " + index  );
+        //     }
+        // }
+        // if(index == -1)
+        // {
+        //     System.out.println("the key is not present in the array ");
+        // }
+
+        // // Q5. Reverse print  the Array -> Array ke elements ko reverse order me print karo.
+        // int[] arr = {1,2,3,4,5};
+        // System.out.println("actual array : ");
+        // for(int i= 0; i<arr.length ;i++)
+        // {
+        //     System.out.println(arr[i]);
+        // }
+        // System.out.println("reversed array : ");
+        // for(int i= arr.length-1; i>=0 ;i--)
+        // {
+        //     System.out.println(arr[i]);
+        // }
+
+        
+    }
+}
